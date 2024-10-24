f4ab07e2dfbd7f8abb94126fc13d3932b22faf8f
Bottesz <botyaring@gmail.com>
2024-10-24 11:01:05 +0200

    kod duplikalas

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ lista = [5,2,3,4,4,5,5,5,1,2,2,3,4,5,5,5,4,3,3]
 jegyek_atlaga=feladat1.jegyek_atlaga(lista)
 print(f"A diákok jegyeinek átlaga: {jegyek_atlaga:.2f}")
 
+
 otosok = feladat2.otosok(lista)
 print(f"Összesen {otosok} ötös van.")
 
